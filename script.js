437be2d4de2fb632841f468cc8d3053dcbe74f39
Tatiana Barreto <cbarretotati@gmail.com>
2024-01-17 15:05:42 -0300

    adicionei info grifinoria

---
+++ script.js
@@ -0,0 +1,3 @@
+function btngrif() {  
+    alert("Fundada por Godrico Grifinória, os alunos desta casa possuem características como coragem, cavalheirismo, ousadia e determinação. Eles geralmente são considerados nobres (de coração), embora às vezes tendem a ser imprudentes. Seu animal emblemático é um leão e suas cores são vermelho e dourado");  
+ } 
